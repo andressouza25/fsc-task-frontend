3eb48f876b7be2889053489042bc3fbf1cdebd7f
Andre Santos <andressouza25@gmail.com>
2026-01-09 18:08:37 -0300

    feat: add Sidebar and SidebarButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Sidebar from './components/Sidebar'
+
 function App() {
   return (
     <div>
+      <Sidebar />
       <h1 className="mt-2 p-6 text-red-500">Task Manager</h1>
     </div>
   )
