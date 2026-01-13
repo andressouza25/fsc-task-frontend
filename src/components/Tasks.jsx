8b9040e0740fafefeccf00cd2af5039724f0345d
Andre Santos <andressouza25@gmail.com>
2026-01-13 15:55:10 -0300

    feat: ensure tasks are listed on Tasks components

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,18 @@ import SunIcon from '../assets/icons/sun.svg?react'
 import CloudSunIcon from '../assets/icons/cloud-sun.svg?react'
 import MoonIcon from '../assets/icons/moon.svg?react'
 import TaskSeparator from './TasksSeparator'
+import { useState } from 'react'
+import TASKS from '../constants/tasks'
+import TaskItem from './TaskItem'
 
 const Tasks = () => {
+  const [tasks] = useState(TASKS)
+
+  const morningTasks = tasks.filter((task) => task.time === 'morning')
+  const afternoonTasks = tasks.filter((task) => task.time === 'afternoon')
+  const eveningTasks = tasks.filter((task) => task.time === 'evening')
+  console.log('TASKS:', TASKS)
+  console.log('tasks state:', tasks)
   return (
     <div className="w-full px-8 py-16">
       {/* { BOTOES} */}
@@ -34,14 +44,23 @@ const Tasks = () => {
       <div className="rounded-xl bg-white p-6">
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<SunIcon />} />
+          {morningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
 
         <div className="my-6 space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudSunIcon />} />
+          {afternoonTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
 
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<MoonIcon />} />
+          {eveningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
