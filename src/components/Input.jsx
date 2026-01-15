6308652bae90d71558e8882416478f2f762224aa
Andre Santos <andressouza25@gmail.com>
2026-01-15 15:30:31 -0300

    chore: add prop-types to Input  component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types'
+import { forwardRef } from 'react'
 
+import InputErrorMessage from './InputErrorMessage'
 import InputLabel from './InputLabel'
 
-const Input = ({ label, errorMessage, ...rest }) => {
+const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
   return (
     <div className="flex flex-col space-y-1 text-left">
       <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
-        className="outline-brand-primary placeholder:text-brand-text-gray border-brand-border rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
+        className="border-brand-border outline-brand-primary placeholder:text-brand-text-gray rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
+        ref={ref}
         {...rest}
       />
-
-      {errorMessage && (
-        <p className="mt-1 text-left text-xs text-red-500">{errorMessage}</p>
-      )}
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
     </div>
   )
-}
+})
 
 Input.displayName = 'Input'
 Input.propTypes = {
